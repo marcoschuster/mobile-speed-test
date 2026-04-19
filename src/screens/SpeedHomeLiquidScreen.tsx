ef948bf4b227a113d15c4bf40884d7335f437cd2
Marco Schuster <marcoschuster27@gmail.com>
2026-04-19 17:17:17 +0200

    Increase bottom position to 450

--- src/screens/SpeedHomeLiquidScreen.tsx
+++ src/screens/SpeedHomeLiquidScreen.tsx
@@ -819,7 +819,7 @@ const SpeedHomeLiquidScreen = () => {
             pointerEvents="none"
             style={{
               position: 'absolute',
-              bottom: 350,
+              bottom: 450,
               left: 0,
               right: 0,
               alignItems: 'center',
