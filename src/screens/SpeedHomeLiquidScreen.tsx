cf7aabb0e0f170c538308d7be6bd49d61367ed7c
Marco Schuster <marcoschuster27@gmail.com>
2026-04-19 17:33:45 +0200

    Move loading button to top: 50 to position near metric slots

--- src/screens/SpeedHomeLiquidScreen.tsx
+++ src/screens/SpeedHomeLiquidScreen.tsx
@@ -819,7 +819,7 @@ const SpeedHomeLiquidScreen = () => {
             pointerEvents="none"
             style={{
               position: 'absolute',
-              bottom: 200,
+              top: 50,
               left: 0,
               right: 0,
               alignItems: 'center',
